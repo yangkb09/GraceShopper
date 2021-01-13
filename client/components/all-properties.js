8d206dd42c061eb7421c197da34d14bdaca10e0c
Katarina Yang <yangkb09@gmail.com>
2021-01-13 11:49:12 -0500

    attempted to test frontend slice 1

--- client/components/all-properties.js
+++ client/components/all-properties.js
@@ -6,6 +6,31 @@ import {Link} from 'react-router-dom'
 // Notice that we're exporting the Allproperties component twice. The named export
 // (below) is not connected to Redux, while the default export (at the very
 // bottom) is connected to Redux. Our tests should cover _both_ cases.
+
+// const properties = [
+//   {
+//     id: 1,
+//     name: 'Century Chalet',
+//     imageUrl: 'https://tinyurl.com/y6636nbd',
+//     address: '1741 Remarkable Rd',
+//     price: 3000000,
+//   },
+//   {
+//     id: 2,
+//     name: 'Holiday House',
+//     imageUrl: 'https://tinyurl.com/y6jeheeg',
+//     address: '2011 Vacation Ct',
+//     price: 6000000,
+//   },
+//   {
+//     id: 3,
+//     name: 'Deer Lodge',
+//     imageUrl: 'https://tinyurl.com/y6jeheeg',
+//     address: '14 Big Bear Rd',
+//     price: 8000000,
+//   },
+// ]
+
 export class AllProperties extends React.Component {
   componentDidMount() {
     this.props.getProperties()
@@ -15,7 +40,7 @@ export class AllProperties extends React.Component {
     if (this.props.properties.length > 0) {
       return (
         <div>
-          {this.props.properties.map((property) => {
+          {this.props.properties.map(property => {
             return (
               <div key={property.id}>
                 <div>
@@ -46,15 +71,15 @@ export class AllProperties extends React.Component {
   }
 }
 
-const mapState = (state) => {
+const mapState = state => {
   return {
-    properties: state.properties,
+    properties: state.properties
   }
 }
 
-const mapDispatch = (dispatch) => {
+const mapDispatch = dispatch => {
   return {
-    getProperties: () => dispatch(_getProperties()),
+    getProperties: () => dispatch(_getProperties())
     // addToCart: (property) => dispatch(property))
   }
 }
