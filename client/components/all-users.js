cbd1c215713633516cb53c04e47056d926bc9c9c
Frankie Goodwin <fmgoodwiniv@gmail.com>
2021-01-17 15:40:24 -0500

    feat: added skeleton all-users component for administrator view

---
+++ client/components/all-users.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {_getUsers} from '../store/user'
+
+export class AllUsers extends React.Component {
+  componentDidMount() {
+    this.props.getUsers()
+  }
+
+  render() {
+    if (this.props.users.length > 0) {
+      return (
+        <div>
+          {this.props.users.map(user => {
+            return (
+              <div key={user.id}>
+                <div>
+                  Name: {user.name}
+                  Email: {user.email}
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <p>
+            Not one user in your database! But then.... how are you an
+            administrator?
+          </p>
+        </div>
+      )
+    }
+  }
+}
+
+const mapState = state => {
+  return {
+    users: state.users
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    getUsers: () => dispatch(_getUsers())
+  }
+}
+
+export default connect(mapState, mapDispatch)(AllUsers)
