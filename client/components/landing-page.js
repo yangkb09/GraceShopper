f611f5e72bc703194a92c864bbd2d3af817fa3d9
Katarina Yang <yangkb09@gmail.com>
2021-01-20 20:32:11 -0500

    style(cart links and final touches)

--- client/components/landing-page.js
+++ client/components/landing-page.js
@@ -8,8 +8,7 @@ export const LandingPage = () => {
   return (
     <div>
       <p className="frontPageText" id="welcomeMsg">
-        Welcome to Virtual Realty, where we will help you find your ideal
-        getaway.
+        Welcome to Virtual Realty, where we help you find your ideal getaway.
       </p>
       <img
         id="landing-page"
